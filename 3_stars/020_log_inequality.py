bfcd0735cb91001db3e666274e8b90a199d60168
cromz22 <tt.pfr4343@gmail.com>
2021-12-03 21:06:23 +0900

    020_log_inequality.py AC

---
+++ 3_stars/020_log_inequality.py
@@ -0,0 +1,14 @@
+from math import log2
+
+
+a, b, c = map(int, input().split())
+
+# if log2(a) < b * log2(c):
+#     print("Yes")
+# else:
+#     print("No")
+
+if a < c**b:
+    print("Yes")
+else:
+    print("No")
