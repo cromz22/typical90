bef2d65f39d0953e5bd6055e68760fa97e171891
cromz22 <tt.pfr4343@gmail.com>
2021-12-05 22:07:57 +0900

    032_atcoder_ekiden.py AC

--- 3_stars/032_atcoder_ekiden.py
+++ 3_stars/032_atcoder_ekiden.py
@@ -1,54 +1,54 @@
 from itertools import permutations
 
 
-def cats_and_dogs(v, cd_grid):
+def cats_and_dogs(v, candd_grid):
     for i in range(len(v) - 1):
-        if cd_grid[v[i]][v[i + 1]]:
+        if candd_grid[v[i]][v[i + 1]]:
             return True
     return False
 
 
 def main():
     n = int(input())
+
     a = [list(map(int, input().split())) for _ in range(n)]
+    # rows reperesent players, columns represent intervals
+
     m = int(input())
+
     if m == 0:
         min_val = 10 ** 18
         val = -1
         for v in permutations(range(n), n):
             val = 0
-            for i, row in enumerate(a):
-                val += row[v[i]]
+            for j in range(n):
+                val += a[v[j]][j]
             min_val = min(min_val, val)
         print(min_val)
 
     else:
-        cats_and_dogs_list = [list(map(int, input().split())) for _ in range(m)]
-        cats_and_dogs_grid = [[False] * n for _ in range(n)]
+        candd_list = [list(map(int, input().split())) for _ in range(m)]
+        # memoize pairs that are cats and dogs
+        candd_grid = [[False] * n for _ in range(n)]
         for i in range(m):
-            cats_and_dogs_grid[cats_and_dogs_list[i][0] - 1][
-                cats_and_dogs_list[i][1] - 1
-            ] = True
-            cats_and_dogs_grid[cats_and_dogs_list[i][1] - 1][
-                cats_and_dogs_list[i][0] - 1
-            ] = True
-
-        print(cats_and_dogs_grid)
+            candd_grid[candd_list[i][0] - 1][candd_list[i][1] - 1] = True
+            candd_grid[candd_list[i][1] - 1][candd_list[i][0] - 1] = True
 
         min_val = 10 ** 18
         val = -1
-        for v in permutations(range(n), n):
-            # if the combination is not possible
-            if cats_and_dogs(v, cats_and_dogs_grid):
+        for v in permutations(range(n)):
+            # if the combination includes a cats-and-dogs pair, skip the combination
+            if cats_and_dogs(v, candd_grid):
                 continue
             else:
                 val = 0
-                for i, row in enumerate(a):
-                    val += row[v[i]]
+                for j in range(n):
+                    val += a[v[j]][j]
                 min_val = min(min_val, val)
 
-        if val == -1:
-            print("-1")
+        # if all the combinations are bad
+        if min_val == 10 ** 18:
+            print(-1)
         else:
             print(min_val)
 
